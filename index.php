67e27a4f2a5cf0428235033ab4b0bdca2fdaff0b
Wesley <wesleyweb3.0@gmail.com>
2023-12-08 00:00:15 -0300

    Add composer

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+require 'vendor/autoload.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>New ...</h1>
+    
+</body>
+</html>
